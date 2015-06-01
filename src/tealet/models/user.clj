c2d50ab3a619273909983b4f2f4aa8734df96419
J. Matt Peterson <jmatt@jmatt.org>
2015-05-31 22:57:34 -0700

    Restart.

--- src/tealet/models/user.clj
+++
@@ -1,1 +0,0 @@
-(ns tealet.models.user)
--- src/tealet/views/main.clj
+++
@@ -1,8 +0,0 @@
-(ns tealet.views.main
-  (:require [tealet.views.common :as common])
-  (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]))
-
-(defpage "/" []
-         (common/layout
-           [:div#content]))
